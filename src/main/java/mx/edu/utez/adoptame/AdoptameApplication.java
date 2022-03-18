50128d1208fd53f3aa2d0b134b49163b403db9fd
Gandy Esau Avila Estrada <avila.gandy@gmail.com>
2022-03-18 04:17:14 -0600

    Proyecto inicializado en Spring 2.6.4 (Java 11) con las siguientes dependencias:  - Spring Web  - Thymeleaf  - Spring Boot DevTools  - Spring Data JPA  - MySQL Driver

---
+++ src/main/java/mx/edu/utez/adoptame/AdoptameApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.adoptame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/adoptame/AdoptameApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.adoptame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
