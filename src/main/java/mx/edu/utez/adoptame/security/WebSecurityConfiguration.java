d7c003122d5bb42cfefc6c9b68172002e3da832c
SpawnSkull <saposuper_14@hotmail.com>
2022-04-03 19:26:22 -0500

    Modificación del archivo de WebSecurity -- nombre de las tablas estandarizados

---
+++ src/main/java/mx/edu/utez/adoptame/security/WebSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package mx.edu.utez.adoptame.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("SELECT nombre, contrasenia FROM usuarios WHERE nombre = ?")
+				.authoritiesByUsernameQuery("SELECT u.nombre, r.titulo FROM rol_usuario AS ru "
+						+ "INNER JOIN usuarios AS u ON u.id = ru.usuario_id "
+						+ "INNER JOIN roles AS r ON r.id = ru.rol_id WHERE u.nombre = ?");
+	}
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/css/**", "/js/**").permitAll()
+
+                // Sección donde se adjuntarán las URL públicas que no ocuparán de autenticación
+                .antMatchers("/", "/signup").permitAll();
+
+                // Sección donde se asignarán permisos a los usuarios dependiendo de su rol
+                //.antMatchers("/mascotas/**").hasAnyAuthority("ROL_VOLUNTARIO")
+                //.antMatchers("/usuarios/**").hasAnyAuthority("ROL_ADOPTADOR")
+                //.antMatchers("/donaciones/**").hasAnyAuthority("ROL_ADOPTADOR")
+                //.antMatchers("/blogs/**").hasAnyAuthority("ROL_ADMINISTRADOR")
+    }
+
+}
