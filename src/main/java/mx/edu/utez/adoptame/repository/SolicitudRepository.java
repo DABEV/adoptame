3273fce9afec6c5277a853251095b244385476c4
SpawnSkull <saposuper_14@hotmail.com>
2022-04-12 19:45:35 -0500

    Agregado función de actualizr estado en solicitud

--- src/main/java/mx/edu/utez/adoptame/repository/SolicitudRepository.java
+++ src/main/java/mx/edu/utez/adoptame/repository/SolicitudRepository.java
@@ -3,13 +3,15 @@ package mx.edu.utez.adoptame.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import mx.edu.utez.adoptame.model.Solicitud;
 
 public interface SolicitudRepository extends JpaRepository<Solicitud, Long> {
-
-    @Modifying
-    @Query(value = "update solicitudes s set s.aprobado = ? where s.id = ?", nativeQuery = true)
-    int updateSolicitudesSetAprobadoForIdNative(String aprobado, long id);
-
+ 
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value = "update solicitudes s set s.aprobado = :aprobado where s.id = :id", nativeQuery = true)
+    void update(@Param("aprobado") String aprobado,@Param("id") long id);
 }
