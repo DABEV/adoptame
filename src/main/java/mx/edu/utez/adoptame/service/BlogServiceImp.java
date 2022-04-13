5e96c8ef62b3b35af906c0bcdcfd2e29a045c2d6
Gustavo Lopez <thered04@outlook.com>
2022-04-13 00:22:46 -0500

    Dialogs de blog agregados

--- src/main/java/mx/edu/utez/adoptame/service/BlogServiceImp.java
+++ src/main/java/mx/edu/utez/adoptame/service/BlogServiceImp.java
@@ -2,29 +2,40 @@ package mx.edu.utez.adoptame.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.edu.utez.adoptame.model.Blog;
+import mx.edu.utez.adoptame.repository.BlogRepository;
 
 @Service
 public class BlogServiceImp implements BlogService {
 
+    @Autowired
+    BlogRepository blogRepository;
+
     @Override
     public List<Blog> listarBlogs() {
-        // TODO Auto-generated method stub
-        return null;
+
+        return blogRepository.findAll();
     }
 
     @Override
     public Blog guardarBlog(Blog blog) {
-        // TODO Auto-generated method stub
-        return null;
+
+        return blogRepository.save(blog);
     }
 
     @Override
     public Blog actualizarBlog(Blog blog) {
-        // TODO Auto-generated method stub
-        return null;
+
+        Blog blogActualizar = blogRepository.getById(blog.getId());
+
+        blogActualizar.setContenido(blog.getContenido());
+        blogActualizar.setTitulo(blog.getTitulo());
+        blogActualizar.setEsPrincipal(blog.getEsPrincipal());
+        
+        return blogRepository.save(blogActualizar);
     }
 
     @Override
@@ -35,8 +46,13 @@ public class BlogServiceImp implements BlogService {
 
     @Override
     public boolean eliminarBlog(Long id) {
-        // TODO Auto-generated method stub
+        Blog blog = blogRepository.getById(id);
+
+        if (blog.getId() != null) {
+            blogRepository.deleteById(id);
+            return true;
+        }
         return false;
     }
-    
+
 }
