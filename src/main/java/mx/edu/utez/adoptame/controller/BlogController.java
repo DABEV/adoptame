d56dd230e31b17748410857dc43eb7c5675c80f3
Gustavo Lopez <thered04@outlook.com>
2022-04-13 18:40:52 -0500

    implementacion de try catch en los controladores y servicios de blog

--- src/main/java/mx/edu/utez/adoptame/controller/BlogController.java
+++ src/main/java/mx/edu/utez/adoptame/controller/BlogController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mx.edu.utez.adoptame.model.Blog;
 import mx.edu.utez.adoptame.service.BlogServiceImp;
+import mx.edu.utez.adoptame.util.ImagenUtileria;
 
 @Controller
 @RequestMapping("/blog")
@@ -20,11 +22,17 @@ public class BlogController {
     @Autowired
     BlogServiceImp blogServiceImp;
 
-    private String blogLista = "redirect:/blog/consultarTodas";
+    private String redirectBlogLista = "redirect:/blog/consultarTodas";
 
     @GetMapping("/consultarTodas")
     public String consultarBlogs(Blog blog, Model model) {
-        model.addAttribute("listaBlog", blogServiceImp.listarBlogs());
+
+        try {
+            model.addAttribute("listaBlog", blogServiceImp.listarBlogs());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "blog/lista";
     }
 
@@ -35,27 +43,72 @@ public class BlogController {
     }
 
     @PostMapping("/guardarBlog")
-    public String guardarBlog(Blog blog, Model model, @RequestParam("imagenMascota") MultipartFile multipartFile) {
+    public String guardarBlog(Blog blog, Model model, RedirectAttributes redirectAttributes,
+            @RequestParam("imagenBlog") MultipartFile multipartFile) {
 
-        blog.setImagen("imagen");
+        try {
+            if (!multipartFile.isEmpty()) {
+                String ruta = "C:/mascotas/img-mascotas/";
+                String nombreImagen = ImagenUtileria.guardarImagen(multipartFile, ruta);
+                if (nombreImagen != null) {
+                    blog.setImagen(nombreImagen);
+                }
+            }
 
-        blogServiceImp.guardarBlog(blog);
+            Blog respuesta = blogServiceImp.guardarBlog(blog);
+            if (respuesta != null) {
+                redirectAttributes.addFlashAttribute("msg_success", "Registro exitoso");
+            } else {
+                redirectAttributes.addFlashAttribute("msg_error", "Registro fallido");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return redirectBlogLista;
 
-        return blogLista;
     }
 
     @PostMapping("/actualizarBlog")
-    public String actualizarBlog(Blog blog) {
-        blog.setImagen("imagen");
+    public String actualizarBlog(Blog blog, Model model, RedirectAttributes redirectAttributes,
+            @RequestParam("imagenBlog") MultipartFile multipartFile) {
 
-        blogServiceImp.actualizarBlog(blog);
-        return blogLista;
+        try {
+
+            if (!multipartFile.isEmpty()) {
+                String ruta = "C:/mascotas/img-mascotas/";
+                String nombreImagen = ImagenUtileria.guardarImagen(multipartFile, ruta);
+                if (nombreImagen != null) {
+                    blog.setImagen(nombreImagen);
+                }
+            }
+
+            Blog respuesta = blogServiceImp.actualizarBlog(blog);
+            if (respuesta != null) {
+                redirectAttributes.addFlashAttribute("msg_success", "Registro exitoso");
+            } else {
+                redirectAttributes.addFlashAttribute("msg_error", "Registro fallido");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return redirectBlogLista;
+
     }
 
     @PostMapping("/borrarBlog")
-    public String borrarBlog(@RequestParam("idBlog") long id) {
-        blogServiceImp.eliminarBlog(id);
-        return blogLista;
+    public String borrarBlog(@RequestParam("idBlog") long id, RedirectAttributes redirectAttributes) {
+        try {
+            Boolean respuestaEliminacion = blogServiceImp.eliminarBlog(id);
+            if (Boolean.TRUE.equals(respuestaEliminacion)) {
+                redirectAttributes.addFlashAttribute("msg_success", "Registro exitoso");
+            } else {
+                redirectAttributes.addFlashAttribute("msg_error", "Registro fallido");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return redirectBlogLista;
     }
 
 }
