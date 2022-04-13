123b1c31d2c1a181aebed54d47e87d7c2e4b95d8
vimillan <violeta.millan036temixco@gmail.com>
2022-04-12 20:34:10 -0500

    Service e implementación de favoritos agregada. Falta unión con mascotas

--- src/main/java/mx/edu/utez/adoptame/service/FavoritoServiceImp.java
+++ src/main/java/mx/edu/utez/adoptame/service/FavoritoServiceImp.java
@@ -1,42 +1,60 @@
 package mx.edu.utez.adoptame.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import mx.edu.utez.adoptame.model.Favorito;
+import mx.edu.utez.adoptame.model.Usuario;
+import mx.edu.utez.adoptame.repository.FavoritoRepository;
 
 @Service
 public class FavoritoServiceImp implements FavoritoService {
 
+    @Autowired
+    FavoritoRepository favoritoRepository;
+
     @Override
-    public List<Favorito> listarFavoritos() {
-        // TODO Auto-generated method stub
-        return null;
+    public List<Favorito> listarFavoritos(Usuario usuario) {
+        return favoritoRepository.findByUsuario(usuario);
     }
 
     @Override
     public Favorito guardarFavorito(Favorito favorito) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Favorito actualizarFavorito(Favorito favorito) {
-        // TODO Auto-generated method stub
-        return null;
+        Favorito favoritoResultante = null;
+        try {
+            favoritoResultante = favoritoRepository.save(favorito);
+        } catch (Exception e) {
+            // log
+        }
+        return favoritoResultante;
     }
 
     @Override
     public Favorito obtenerFavorito(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        Favorito favoritoResultante = null;
+        Optional<Favorito> favoritoOpcional = favoritoRepository.findById(id);
+
+        if (favoritoOpcional.isPresent()) {
+            favoritoResultante = favoritoOpcional.get();
+        }
+        return favoritoResultante;
     }
 
     @Override
     public boolean eliminarFavorito(Long id) {
-        // TODO Auto-generated method stub
+        try {
+            Optional<Favorito> favoritoOpcional = favoritoRepository.findById(id);
+            if (favoritoOpcional.isPresent()) {
+                favoritoRepository.deleteById(id);
+            }
+            return true;
+        } catch (Exception e) {
+            // Log
+        }
         return false;
     }
-    
+
 }
