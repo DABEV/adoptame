9fb6b01b2e829e699e53ff1282e4b836a83ea0d5
Violeta Millán <67834307+vimillan@users.noreply.github.com>
2022-04-17 13:45:53 -0500

    Corecciones en landing y se retiraron comentarios en usuarioController

--- src/main/java/mx/edu/utez/adoptame/controller/UsuarioController.java
+++ src/main/java/mx/edu/utez/adoptame/controller/UsuarioController.java
@@ -45,15 +45,18 @@ public class UsuarioController {
 
     @GetMapping("/actualizar")
     @PreAuthorize("hasAuthority('ROL_ADMINISTRADOR') or hasAuthority('ROL_VOLUNTARIO') or hasAuthority('ROL_ADOPTADOR')")
-    public String actualizar () {
-        return REDIRECT_MI_CUENTA;    
+    public String actualizar() {
+        return REDIRECT_MI_CUENTA;
     }
 
     @PostMapping("/actualizar")
     @PreAuthorize("hasAuthority('ROL_ADMINISTRADOR') or hasAuthority('ROL_VOLUNTARIO') or hasAuthority('ROL_ADOPTADOR')")
-    public String actualizar (Authentication authentication, RedirectAttributes redirectAttributes, @RequestParam("contrasena") String contrasena, @RequestParam("nuevaContrasena") String nuevaContrasena, @RequestParam("repetirContrasena") String repetirContrasena, @Valid @ModelAttribute("usuario") UsuarioDto usuarioDto, BindingResult result) {
+    public String actualizar(Authentication authentication, RedirectAttributes redirectAttributes,
+            @RequestParam("contrasena") String contrasena, @RequestParam("nuevaContrasena") String nuevaContrasena,
+            @RequestParam("repetirContrasena") String repetirContrasena,
+            @Valid @ModelAttribute("usuario") UsuarioDto usuarioDto, BindingResult result) {
         try {
-            if (result.hasErrors())  {
+            if (result.hasErrors()) {
                 redirectAttributes.addFlashAttribute(MSG_ERROR, "Error en algunos campos, favor de verificarlos.");
                 return VIEW_MI_CUENTA;
             }
@@ -61,14 +64,14 @@ public class UsuarioController {
             Usuario usuario = usuarioServiceImp.buscarPorCorreo(authentication.getName());
 
             // Caso donde quieren cambiar la contraseña
-            if (contrasena != null  && (!contrasena.isEmpty() || !contrasena.isBlank())) {
-                if (nuevaContrasena != null && nuevaContrasena.equals(repetirContrasena) && passwordEncoder.matches(contrasena, usuario.getContrasena())) {
-                    System.out.println("Actualice la contraseña");
+            if (contrasena != null && (!contrasena.isEmpty() || !contrasena.isBlank())) {
+                if (nuevaContrasena != null && nuevaContrasena.equals(repetirContrasena)
+                        && passwordEncoder.matches(contrasena, usuario.getContrasena())) {
                     // Si la nueva contraseña coincide con la repetición
-                    usuario.setContrasena(passwordEncoder.encode(nuevaContrasena));    
+                    usuario.setContrasena(passwordEncoder.encode(nuevaContrasena));
                 } else {
-                    System.out.println("Las contraseñas no coinciden, favor de intentarlo de nuevo.");
-                    redirectAttributes.addFlashAttribute(MSG_ERROR, "Las contraseñas no coinciden, favor de intentarlo de nuevo.");
+                    redirectAttributes.addFlashAttribute(MSG_ERROR,
+                            "Las contraseñas no coinciden, favor de intentarlo de nuevo.");
                 }
             }
 
@@ -80,11 +83,11 @@ public class UsuarioController {
                 usuario.setTelefono(usuarioDto.getTelefono().replaceAll("[\\s\\(\\)\\-]+", ""));
 
                 if (usuarioServiceImp.guardarUsuario(usuario) != null) {
-                    System.out.println("msg_success Información del usuario actualizada correctamente.");
-                    redirectAttributes.addFlashAttribute("msg_success", "Información del usuario actualizada correctamente.");                
+                    redirectAttributes.addFlashAttribute("msg_success",
+                            "Información del usuario actualizada correctamente.");
                 } else {
-                    System.out.println("Error al actualizar el usuario, favor de intentarlo más tarde.");
-                    redirectAttributes.addFlashAttribute(MSG_ERROR, "Error al actualizar el usuario, favor de intentarlo más tarde.");
+                    redirectAttributes.addFlashAttribute(MSG_ERROR,
+                            "Error al actualizar el usuario, favor de intentarlo más tarde.");
                 }
             }
         } catch (Exception e) {
@@ -102,10 +105,10 @@ public class UsuarioController {
             Usuario usuario = usuarioServiceImp.buscarPorCorreo(authentication.getName());
 
             UsuarioDto usuarioDto = modelMapper.map(usuario, UsuarioDto.class);
-            
+
             model.addAttribute("usuario", usuarioDto);
 
-            if (usuarioDto != null){
+            if (usuarioDto != null) {
                 model.addAttribute("usuario", usuarioDto);
                 return VIEW_MI_CUENTA;
             }
