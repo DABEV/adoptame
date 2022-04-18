df43d383d56529bb829d5d3ec1a9388b4a98070f
SpawnSkull <saposuper_14@hotmail.com>
2022-04-18 15:16:13 -0500

    Correción de inserts SQL

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -4,10 +4,10 @@ INSERT INTO colores (nombre) VALUES ('Blanco'), ('Café'), ('Gris'), ('Negro'), 
 
 INSERT INTO caracteres (nombre) VALUES ('Activo'), ('Independiente'), ('Juguetón'), ('Protector'), ('Ruidoso'), ('Tímido'), ('Tranquilo'), ('Amoroso');
 
-INSERT INTO roles(id, authority) VALUES (1, 'ROL_ADMINISTRADOR'), (2, 'ROL_VOLUNTARIO'), (3, 'ROL_ADOPTADOR');
+INSERT INTO roles(id, nombre) VALUES (1, 'ROL_ADMINISTRADOR'), (2, 'ROL_VOLUNTARIO'), (3, 'ROL_ADOPTADOR');
 
 -- Configura un administrador
-INSERT INTO usuario (id, nombre, apellidos, habilitado, correo, contrasenia, dirección, fecha_registro, telefono) VALUES 
+INSERT INTO usuarios (id, nombre, apellidos, habilitado, correo, contrasenia, dirección, fecha_registro, telefono) VALUES 
 (1, '', '', 1, '', '', '', NOW(), '');
 
 -- Asignale el rol
